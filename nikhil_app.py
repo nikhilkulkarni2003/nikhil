6650ebf71f830eabb32b1fdb03ea2a70057bb66a
nikhilkulkarni <nikhilkulkarninikhil60@gmail.com>
2025-03-13 10:11:15 +0530

    Update nikhil_app.py

--- nikhil_app.py
+++ nikhil_app.py
@@ -1,1 +1,13 @@
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+early_stopping = EarlyStopping(monitor='val_accuracy', patience=10, restore_best_weights=True)
+checkpoint_path = "./cp.weights.h5"
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+model_rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=15, verbose=True)
